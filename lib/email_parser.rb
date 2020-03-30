8bdc45bcdc5af57c8608e1f58938253a6e165766
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-30 18:51:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@
 class EmailAddressParser 
   attr_accessor :email_addresses 
   def initialize(email_addresses)
-    @email_addresses = (","), (" ")
+    @email_addresses = [(","), (" ")]
   end 
   def parse 
     @email_addresses.uniq!
